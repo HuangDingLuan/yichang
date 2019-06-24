0796cdf78c82e4b1dde5b77d9c54191ea790f334
huangdingluan <316943766@qq.com>
2019-06-24 22:47:15 +0800

    2

--- src/异常/yichang.java
+++ src/异常/yichang.java
@@ -10,10 +10,28 @@ class MyException extends Throwable{
 		return String.format("\"%s\"", Thread.currentThread().getName())+"For input string:"+super.getMessage();
 	}
 }
+class MyTool{
+	public static int ParseInt(String str) throws MyException{
+		int result=0;
+		int i;
+		for(i=0;i<str.length();i++) {
+			char ch=str.charAt(i);
+			result=result*10+ch-'0';
+			if(ch>='0'&&ch<='9')result=result*10+ch-'0';
+			else throw new MyException(str);
+		}
+		return result;
+	}
+}
 public class yichang {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		try {
+			System.out.println(MyTool.ParseInt("\"345a\""));
+		}catch(MyException e) {
+			e.printStackTrace();
+		}
 
 	}
 
